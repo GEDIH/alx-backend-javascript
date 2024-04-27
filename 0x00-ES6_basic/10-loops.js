f2645b72fb2ccff47b7c9626b0eb3427c1f5c462
HAMBISA ADAMU <117825147+GEDIH@users.noreply.github.com>
2024-04-27 09:59:25 +0300

    Update 10-loops.js

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,1 @@
-export default function appendToEachArrayValue(array, appendString) {
-  const newArray = [];
-  for (const idx of array) {
-    newArray.push(appendString + idx);
-  }
-  return newArray;
-}
+
