3d718e60f3c39a15a52ac4fa741906bc97c20947
HAMBISA ADAMU <117825147+GEDIH@users.noreply.github.com>
2024-06-18 13:11:22 +0300

    AppController.js

--- 0x05-Node_JS_basic/full_server/controllers/AppController.js
+++ 0x05-Node_JS_basic/full_server/controllers/AppController.js
@@ -1,6 +1,6 @@
 /**
  * Contains the miscellaneous route handlers.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ * @author Hambisa Adamu <https://github.com/GEDIH>
  */
 class AppController {
   static getHomepage(request, response) {
