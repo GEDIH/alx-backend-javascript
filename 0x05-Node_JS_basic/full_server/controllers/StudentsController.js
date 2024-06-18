1fd03fdb8838d059a7d5b6b3ac77ee2ca01a76e7
HAMBISA ADAMU <117825147+GEDIH@users.noreply.github.com>
2024-06-18 13:11:50 +0300

    StudentsController.js

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -7,7 +7,7 @@ const VALID_MAJORS = ['CS', 'SWE'];
 
 /**
  * Contains the student-related route handlers.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ * @author Hambisa Adamu <https://github.com/GEDIH>
  */
 class StudentsController {
   static getAllStudents(request, response) {
